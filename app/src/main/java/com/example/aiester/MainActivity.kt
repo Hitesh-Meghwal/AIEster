e7a2ed1b4c35dc1925faa5bc94d0357c8223069d
Hitesh Meghwal <129782600+Hitesh-Meghwal@users.noreply.github.com>
2023-04-16 01:04:10 +0530

    Translator UI Done

--- app/src/main/java/com/example/aiester/MainActivity.kt
+++ app/src/main/java/com/example/aiester/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.aiester
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        translator activity will be open
+        val translatorbtn = findViewById<Button>(R.id.translator)
+        translatorbtn.setOnClickListener {
+            val translatorintent = Intent(this,Translator::class.java)
+            startActivity(translatorintent)
+        }
     }
 }
